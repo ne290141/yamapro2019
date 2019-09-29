306ddc4426b3efa1120db51272595bf000eb43d1
ne290141 <ne290141@senshu-u.jp>
2019-09-29 20:29:27 +0900

    create front image select program

---
+++ checkbuilding/programs/front/create_npy.py
@@ -0,0 +1,44 @@
+from PIL import Image
+import os, glob
+import numpy as np
+import random, math
+
+root_dir = "../../images/front"
+
+categories = ["2", "9", "10"]
+
+X = []
+Y = []
+
+def make_sample(files):
+    global X, Y
+    X = []
+    Y = []
+    for cat, fname in files:
+        add_sample(cat, fname)
+    return np.array(X), np.array(Y)
+
+def add_sample(cat, fname):
+    img = Image.open(fname)
+    img = img.convert("RGB")
+    img = img.resize((150, 150))
+    data = np.asarray(img)
+    X.append(data)
+    Y.append(cat)
+
+allfiles = []
+
+for idx, cat in enumerate(categories):
+    image_dir = root_dir+"/"+cat
+    files = glob.glob(image_dir + "/*.jpg")
+    for f in files:
+        allfiles.append((idx, f))
+
+random.shuffle(allfiles)
+th = math.floor(len(allfiles) * 0.8)
+train = allfiles[0:th]
+test  = allfiles[th:]
+X_train, y_train = make_sample(train)
+X_test, y_test = make_sample(test)
+xy = (X_train, X_test, y_train, y_test)
+np.save("../../data/front/uni_data.npy", xy)
